17d8597a6e5d831a328958cf39925865cf0ffa61
Batool Fatima <batoolfatima2135@gmail.com>
2023-08-26 03:58:06 +0500

    Solve rubocop offenses

--- person.rb
+++ person.rb
@@ -1,11 +1,12 @@
 require './nameable'
 require './capitilize_decorator'
 require './trimmer_decorator'
-require './rental.rb'
-require './book.rb'
 
 
 class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
@@ -14,8 +15,6 @@ class Person < Nameable
     @parent_permission = parent_permission
     @rentals = []
   end
-  attr_accessor :name, :age
-  attr_reader :id
 
   def can_use_services?
     @age >= 18 || @parent_permission
@@ -25,21 +24,9 @@ class Person < Nameable
     @name
   end
 
-  def rentals
-    @rentals
-  end
-
   private
 
   def of_age?
     @age && @age >= 18
   end
-
- 
 end
-
-book1 = Book.new("book1", "author1")
-person1 = Person.new(12)
-
-rental1 = Rental.new("2017-12-22", book1, person1)
-
--- student.rb
+++ student.rb
@@ -1,27 +1,18 @@
 require './person'
-require './classroom.rb'
 
 class Student < Person
-  attr_accessor :classroom
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(age, name)
+  attr_reader :classroom
 
+  def initialize(age, name = 'Unknown')
+    super(age, name)
   end
 
-   def classroom=(classroom)
+  def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
+
   def play_hooky
     '¯\(ツ)/¯'
   end
-
 end
-class1 = Classroom.new("maths")
-student1 = Student.new(12)
-class1.add_student(student1)
-puts student1.classroom.label
-student2 = Student.new(15)
-student2.classroom = class1
-puts student2.classroom.label
-puts class1.students.map { |student| student.age } 
