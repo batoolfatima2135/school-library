0ed1a95481a1f72a77fc1fd75f1cb3f872c77442
Batool Fatima <batoolfatima2135@gmail.com>
2023-08-26 04:03:38 +0500

    Remove rubocop offenses

--- main.rb
+++ main.rb
@@ -16,7 +16,6 @@ book1 = Book.new('book1', 'author1')
 person1 = Person.new(12)
 rental1 = Rental.new('2017-12-22', book1, person1)
 
-
 puts rental1.book.title
 puts rental1.person.age
 puts student2.classroom.label
