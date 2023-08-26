8f667ba11d38a4f25ba141d05861c0307020040f
Batool Fatima <batoolfatima2135@gmail.com>
2023-08-27 01:51:36 +0500

    Update files

--- student.rb
+++ student.rb
@@ -3,15 +3,16 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknown')
+  def initialize(age, name = 'Unknown', parent_permission)
     super(age, name)
+    @parent_permission = parent_permission
   end
 
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
-
+  
   def play_hooky
     '¯\(ツ)/¯'
   end
