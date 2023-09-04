b011656d17f5f0fdd0c9a5409a3eeb988f45492d
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-04 20:00:49 +0500

    Solve offenses

--- spec/basedecorator_spec.rb
+++ spec/basedecorator_spec.rb
@@ -5,7 +5,7 @@ describe BaseDecorator do
     it 'should return name' do
       nameable_double = double(Nameable)
       allow(nameable_double).to receive(:correct_name).and_return('John')
-      base_decorator = BaseDecorator.new(Nameable_double)
+      base_decorator = BaseDecorator.new(nameable_double)
       expect(base_decorator.correct_name).to eq('John')
     end
   end
--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,5 +1,5 @@
-require_relative '../classroom'
-require_relative '../student'
+require '././classroom'
+require '././student'
 
 describe Classroom do
   context 'It should create classroom and add student' do
