1ec6e8a816937f67e47f1da34ee84d8350284a4d
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-04 19:35:09 +0500

    Resolve rubocop offenses

--- rental_manager.rb
+++ rental_manager.rb
@@ -11,7 +11,7 @@ class RentalManager
     book = all_books[book_index]
     person = all_persons[person_index]
     Rental.new(date, book, person)
-    @all_rentals.push({ date: date, book_index: book_index, person_index: person_index })
+    @all_rentals.push({ date:, book_index:, person_index: })
   end
 
   def get_rental(id, all_persons)
