4e57ffea2454edf6051372a1e56159e1ce86bdd7
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-04 14:51:32 +0500

    Make changes to tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,40 +1,39 @@
-require  '././book.rb'
-require  '././rental.rb'
+require '././book'
+require '././rental'
 
-describe Book do 
-  context "should create object correctly" do  
-    before(:each) do 
-      @book = Book.new "title", "author"
+describe Book do
+  context 'should create object correctly' do
+    before(:each) do
+      @book = Book.new 'title', 'author'
     end
 
-    it "Create Book object" do
-      expect(@book).to be_instance_of Book  
+    it 'Create Book object' do
+      expect(@book).to be_instance_of Book
     end
 
-    it "Have parameters title and author" do
-      expect(@book).to have_attributes(title: 'title') 
-      expect(@book).to have_attributes(author: 'author') 
+    it 'Have parameters title and author' do
+      expect(@book).to have_attributes(title: 'title', author: 'author')
     end
-  end 
+  end
 
-  context "should add rentals" do  
-    before(:each) do 
-      @book = Book.new "title", "author"
-      @person = double('Person') 
+  context 'should add rentals' do
+    before(:each) do
+      @book = Book.new 'title', 'author'
+      @person = double('Person')
+      @date = '12/12/12'
+      @rental = double('Rental')
     end
 
-    it "should send rental class correct parameters" do  
-      date = '12/12/12'
-      rental_double = double('Rental')
-      allow(Rental).to receive(:new).with(date, @book, @person)
-      @book.add_rental(date, @person)    
-    end 
+    it 'should send rental class correct parameters' do
+     
+      allow(Rental).to receive(:new).with(@date, @book, @person)
+      @book.add_rental(@date, @person)
+    end
 
-    it "should get rental object" do  
-      date = '12/12/12'
-      rental_double = double('Rental')
-      allow(Rental).to receive(:new).and_return(rental_double)
-      @book.add_rental(date, @person) 
-    end 
-  end  
+    it 'should get rental object' do
+\
+      allow(Rental).to receive(:new).and_return(@rental)
+      @book.add_rental(@date, @person)
+    end
+  end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,22 +1,46 @@
 require '././person'
 
 describe Person do
-
-context "Create objects correctly" do
-  before(:each) do 
-      @person = Person.new "age", "name"
+  context 'Create objects correctly' do
+    it 'Creates an object of the Person class' do
+      expect(described_class).to receive(:new).with(25, 'John')
+      Person.new(25, 'John')
     end
 
-  it "Create object of person class" do
-      expect(@person).to be_instance_of Person
+    it 'Passes arguments correctly' do
+      person = described_class.new(25, 'John')
+      expect(person).to have_attributes(age: 25, name: 'John')
+    end
   end
 
-  it "Pass aurgument correctly" do
-    expect(@person).to have_attributes(age: "age")
-    expect(@person).to have_attributes(name: "name")
+  context '#can_use_services?' do
+    it 'Returns true if person is of age' do
+      person = described_class.new(18, 'Alice')
+      expect(person.can_use_services?).to be(true)
+    end
+
+    it 'Returns false if person is not of age' do
+      person = described_class.new(15, 'Bob', parent_permission: false)
+      expect(person.can_use_services?).to be(false)
+    end
   end
 
-end
+  context 'Should add rentals correctly' do
+    before(:each) do
+      @person = described_class.new(25, 'John')
+      @book = double('Book')
+      @date = '12/12/12'
+    end
 
-context ""
+    it 'Sends Rental.new with correct parameters' do
+      expect(Rental).to receive(:new).with(@date, @book, @person)
+      @person.add_rental(@date, @book)
+    end
+
+    it 'Creates a rental object correctly' do
+      rental = double('Rental')
+      allow(Rental).to receive(:new).and_return(rental)
+      expect(@person.add_rental(@date, @book)).to eq(rental)
+    end
+  end
 end
