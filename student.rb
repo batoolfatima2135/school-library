7ff96d958f241e71d0986e980df74f89803699ae
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-04 14:13:53 +0500

    Update student class

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, parent_permission, name = 'Unknown')
+  def initialize(age, name, parent_permission: true)
     super(age, name)
     @parent_permission = parent_permission
   end
