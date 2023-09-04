8bec091bfac4c1e1831175b760e67ad8159a3030
moFuhidy <mo7ammed9290@gmail.com>
2023-09-04 16:54:58 +0300

    Fix the capitalize testing

--- spec/capitalize_spec.rb
+++ spec/capitalize_spec.rb
@@ -2,9 +2,10 @@ require_relative '../capitilize_decorator.rb'
 
 describe CapitalizeDecorator do
   context 'Passing a name' do
-    capitalizer = CapitalizeDecorator.new
-    it 'should return word with first capital letter' do
-      expect(capitalizer.correct_name('mohammed')).to eq 'Mohammed'
+   it 'should return word with first capital letter' do
+      base_decorator_double = double('BaseDecorator', correct_name: 'mohammed')
+      capitalizer = CapitalizeDecorator.new(base_decorator_double)
+      expect(capitalizer.correct_name).to eq 'MOHAMMED'
     end
   end
 end
