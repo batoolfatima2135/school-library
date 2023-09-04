1a8c3368fcc8e8970831b31049f646dae92a2980
moFuhidy <mo7ammed9290@gmail.com>
2023-09-04 16:56:30 +0300

    Fix teacher testing

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,8 +1,8 @@
-require_relative '../teacher'
+require '././teacher'
 
 describe Teacher do
   context 'When add teacher information' do
-    teacher = Teacher.new(age: 29, specialization: 'Programming', name: 'Moha')
+    teacher = Teacher.new(29,  'Moha', 'Programming', nil)
 
     it 'creates a new teacher with the given parameters' do
       expect(teacher).to be_instance_of Teacher
