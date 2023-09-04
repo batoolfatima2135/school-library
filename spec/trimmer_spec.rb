a0dd4f13b1e9775aed78310e54b7dfea2d5ebfc9
moFuhidy <mo7ammed9290@gmail.com>
2023-09-04 16:57:37 +0300

    Fix trimmer testing

--- spec/trimmer_spec.rb
+++ spec/trimmer_spec.rb
@@ -1,10 +1,11 @@
-require_relative '../trimmer_decorator.rb'
+require '././trimmer_decorator.rb'
 
 describe TrimmerDecorator do
   context 'Passing a name' do
-    trimmer = TrimmerDecorator.new
     it 'should slice if sting is more than 10 char' do
-      expect(trimmer.correct_name('mohammedfuhidy')).to eq 'mohammedfu'
+      base_decorator_double = double('BaseDecorator', correct_name: 'mohammedfuhidy')
+      trimmer = TrimmerDecorator.new(base_decorator_double)
+      expect(trimmer.correct_name).to eq 'mohammedfu'
       expect(trimmer.correct_name.length).to be <= 10
     end
   end
